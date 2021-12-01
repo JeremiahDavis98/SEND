e3cb0792970d508b2b533d487a3fa1880242b479
Jeremiah Davis <47806981+JeremiahDavis98@users.noreply.github.com>
2021-11-30 20:14:50 -0600

    L

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__, static_folder='./build', static_url_path='/')
 
 @app.route('/')
 def index():
-    return "hi"
+    return app.send_static_file('index.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Page from './Page';
 
 function App() {
   return (
     <div className="App">
-      <p>Hello World</p>
+      <Page />
     </div>
   );
 }
