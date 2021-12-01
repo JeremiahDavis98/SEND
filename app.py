4febea102500a8b4caa2df7ed2e3a55c2be6739d
Jeremiah Davis <47806981+JeremiahDavis98@users.noreply.github.com>
2021-11-30 20:35:56 -0600

    L

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__, static_folder='./build', static_url_path='/')
 def index():
     return app.send_static_file('index.html')
 
+@app.route('/HomePage')
+def HomePage():
+    return url_for('static', filename = 'HomePage.js')
+    
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=False, port=os.environ.get('PORT', 80))
