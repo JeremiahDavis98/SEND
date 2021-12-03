3df8ab7812cad5753cdcabfe7eb516f0e7f85be7
Jeremiah Davis <47806981+JeremiahDavis98@users.noreply.github.com>
2021-12-03 04:32:44 -0600

    router working ish

--- src/App.js
+++ src/App.js
@@ -1,65 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import Login from './Pages/Login/Login';
 import Auth from './Auth';
 import React, { useState } from "react";
 import Navbar from "react-bootstrap/Navbar";
-import Nav from "react-bootstrap/Nav";
-import { LinkContainer } from "react-router-bootstrap";
-import Routes from './Routes';
-import { AppContext } from './Pages/lib/contextLib';
+//import Nav from "react-bootstrap/Nav";
+//import { LinkContainer } from "react-router-bootstrap";
+//import Routes from './Routes';
+//import { AppContext } from './Pages/lib/contextLib';
+import HomePage from './Pages/HomePage/HomePage';
+import Downloads from './Pages/Downloads/Downloads';
+import Signup from './Pages/Signup/Signup';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+import { Redirect } from 'react-router';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState();//useState(Auth.isAuthenticated())
+  const [isLoggedIn, setIsLoggedIn] = useState();//useState(Auth.isAuthenticated()) 
+  const [isAuth, setAuth] = useState();
+  function requireAuth(component){
+    <Redirect to = "/login" />;
+  }
+  function updateApp(){
+    setAuth(!isAuth);
+  }
   return (
     <div className="App">
-      <Navbar collapseOnSelect bg = "light" expand = "md" className = "mb-3">
-        <LinkContainer to = "/HomePage">
-          <Navbar.Brand className = "Page Select">
-            EE461L - Team SEND Project
-          </Navbar.Brand>
-        </LinkContainer>
-
-        <Navbar.Toggle/>
-        <Navbar.Collapse>
-          <Nav activeKey = {window.location.pathname}>
-            {isLoggedIn ? (
-              <>
-                <LinkContainer to = "/Signup">
-                  <Nav.Link>Signup</Nav.Link>
-                </LinkContainer> <br></br>
-                <LinkContainer to = "/Downloads">
-                  <Nav.Link>Dataset Downloads</Nav.Link>
-                </LinkContainer> <br></br>
-                <LinkContainer to = "/api/logout">
-                  <Nav.Link>Logout</Nav.Link>
-                </LinkContainer>
-              </>
-            )
-            : (
-              <>
-              <LinkContainer to = "/Login">
-                <Nav.Link>Login</Nav.Link>
-              </LinkContainer> <br></br>
-              <LinkContainer to = "/Signup">
-                <Nav.Link>Signup</Nav.Link>
-              </LinkContainer> <br></br>
-              <LinkContainer to = "/Downloads">
-                <Nav.Link>Dataset Downloads</Nav.Link>
-              </LinkContainer> <br></br>
-              <LinkContainer to = "/api/logout">
-                <Nav.Link>Logout</Nav.Link>
-              </LinkContainer>
-              </>
-            )}
-
-
-          </Nav>
-        </Navbar.Collapse>
-      </Navbar>
-      <AppContext.Provider value = {{isLoggedIn, setIsLoggedIn}}>
-        <Routes />
-      </AppContext.Provider>
+      <Router>
+        <Navbar isAuth={isAuth}
+          updateApp={updateApp} />
+        <Switch>
+          <Route path= "/" exact component = {HomePage} />
+          <Route path= "/Login" exact render = {() => <Login updateApp ={updateApp} />} />
+          <Route path= "/Signup" exact render = {() => <Signup updateApp={updateApp}/>} />
+          <Route path= "/" exact component = {Downloads} />
+        </Switch>
+      </Router>
     </div>
   );
 }
