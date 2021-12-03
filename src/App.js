e4c5b2167e837cbfa031b6b275d0c1f0f0fe3f9b
Jeremiah Davis <47806981+JeremiahDavis98@users.noreply.github.com>
2021-12-03 03:20:52 -0600

    navbar attempt

--- src/App.js
+++ src/App.js
@@ -3,12 +3,62 @@ import './App.css';
 import Login from './Pages/Login/Login';
 import Auth from './Auth';
 import React, { useState } from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Nav from "react-bootstrap/Nav";
+import { LinkContainer } from "react-router-bootstrap";
+import Routes from './Routes';
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(Auth.isAuthenticated())
   return (
     <div className="App">
-      <Login />
+      <Navbar collapseOnSelect bg = "light" expand = "md" className = "mb-3">
+        <LinkContainer to = "/HomePage">
+          <Navbar.Brand className = "Page Select">
+            EE461L - Team SEND Project
+          </Navbar.Brand>
+        </LinkContainer>
+
+        <Navbar.Toggle/>
+        <Navbar.Collapse>
+          <Nav activeKey = {window.location.pathname}>
+            {isLoggedIn ? (
+              <>
+                <LinkContainer to = "/Signup">
+                  <Nav.Link>Signup</Nav.Link>
+                </LinkContainer> <br></br>
+                <LinkContainer to = "/Downloads">
+                  <Nav.Link>Dataset Downloads</Nav.Link>
+                </LinkContainer> <br></br>
+                <LinkContainer to = "/api/logout">
+                  <Nav.Link>Logout</Nav.Link>
+                </LinkContainer>
+              </>
+            )
+            : (
+              <>
+              <LinkContainer to = "/Login">
+                <Nav.Link>Login</Nav.Link>
+              </LinkContainer> <br></br>
+              <LinkContainer to = "/Signup">
+                <Nav.Link>Signup</Nav.Link>
+              </LinkContainer> <br></br>
+              <LinkContainer to = "/Downloads">
+                <Nav.Link>Dataset Downloads</Nav.Link>
+              </LinkContainer> <br></br>
+              <LinkContainer to = "/api/logout">
+                <Nav.Link>Logout</Nav.Link>
+              </LinkContainer>
+              </>
+            )}
+
+
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+      <AppContext.Provider value = {{isLoggedIn, setIsLoggedIn}}>
+        <Routes />
+      </AppContext.Provider>
     </div>
   );
 }
